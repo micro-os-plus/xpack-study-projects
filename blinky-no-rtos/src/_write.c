c450b8de6271fbd4cab47fbf78ef3337f602d81f
Liviu Ionescu <ilg@livius.net>
2020-11-25 23:19:43 +0200

    _write.c: update to OS_USE_SEMIHOSTING_SYSCALLS

--- blinky-no-rtos/src/_write.c
+++ blinky-no-rtos/src/_write.c
@@ -26,7 +26,7 @@
  */
 
 // Do not include on semihosting and when freestanding!
-#if !defined(OS_USE_SEMIHOSTING) && !(__STDC_HOSTED__ == 0)
+#if !defined(OS_USE_SEMIHOSTING_SYSCALLS) && !(__STDC_HOSTED__ == 0)
 
 // ----------------------------------------------------------------------------
 
@@ -69,4 +69,4 @@ _write (int fd __attribute__((unused)), const char* buf __attribute__((unused)),
 
 // ----------------------------------------------------------------------------
 
-#endif // !defined(OS_USE_SEMIHOSTING) && !(__STDC_HOSTED__ == 0)
+#endif // !defined(OS_USE_SEMIHOSTING_SYSCALLS) && !(__STDC_HOSTED__ == 0)
