2cf5dae2548545b7639b3307a6d3b78000aa563a
Liviu Ionescu <ilg@livius.net>
2021-01-08 13:00:53 +0200

    add blinky-rtos, functional on stm32f4-discovery

---
+++ blinky-rtos/platform-stm32f4discovery/src/initialize-hardware.cpp
@@ -0,0 +1,75 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2017 Liviu Ionescu.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <micro-os-plus/platform.h>
+#include <micro-os-plus/startup/hooks.h>
+
+// ----------------------------------------------------------------------------
+
+// Called early, before copying .data and clearing .bss.
+// Should initialize the clocks and possible other RAM areas.
+void
+os_startup_initialize_hardware_early (void)
+{
+  ; // None so far.
+}
+
+#pragma GCC diagnostic push
+// #pragma GCC diagnostic ignored "-Wsign-conversion"
+
+extern "C"
+{
+  void SystemClock_Config(void);
+  void MX_GPIO_Init(void);
+}
+
+// Called before running the static constructors.
+void
+os_startup_initialize_hardware (void)
+{
+  // Initialise the HAL Library; it must be the first function
+  // to be executed before the call of any HAL function.
+  HAL_Init();
+
+  // Enable HSE Oscillator and activate PLL with HSE as source
+  SystemClock_Config();
+
+  // Call the CSMSIS system clock routine to store the clock frequency
+  // in the SystemCoreClock global RAM location.
+  SystemCoreClockUpdate();
+
+  // Initialize all configured peripherals.
+  MX_GPIO_Init();
+
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+}
+
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------------
