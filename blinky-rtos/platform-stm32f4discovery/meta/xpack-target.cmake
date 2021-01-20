267be7eeff66aad94a6615f2c781cc491ab3b432
Liviu Ionescu <ilg@livius.net>
2021-01-20 23:23:46 +0200

    blinky-rtos: rename target to monolithic

--- blinky-rtos/platform-stm32f4discovery/meta/xpack-target.cmake
+++ blinky-rtos/platform-stm32f4discovery/meta/xpack-target.cmake
@@ -17,7 +17,7 @@ set(XPACK_PLATFORM_COMPILE_DEFINITION "PLATFORM_STM32F4DISCOVERY")
 set(XPACK_DEVICE_COMPILE_DEFINITION "STM32F407xx")
 set(XPACK_DEVICE_FAMILY_COMPILE_DEFINITION "STM32F4")
 
-function(target_sources_platform_stm32f4discovery target)
+function(target_sources_platform target)
 
   get_filename_component(PARENT_DIR ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
 
@@ -56,13 +56,14 @@ function(target_sources_platform_stm32f4discovery target)
   )
 endfunction()
 
-function(target_include_directories_platform_stm32f4discovery target)
+function(target_include_directories_platform target)
+
   get_filename_component(PARENT_DIR ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
 
   target_include_directories(
-    xpack-target
+    ${target}
+
     PRIVATE
-    
       ${PARENT_DIR}/include
       ${PARENT_DIR}/stm32cubemx/Core/Inc
       ${PARENT_DIR}/stm32cubemx/Drivers/CMSIS/Device/ST/STM32F4xx/Include
@@ -74,82 +75,93 @@ function(target_include_directories_platform_stm32f4discovery target)
   )
 endfunction()
 
-target_compile_definitions(
-  xpack-target
-  PRIVATE
-  
-    ${XPACK_PLATFORM_COMPILE_DEFINITION}
-    ${XPACK_DEVICE_COMPILE_DEFINITION}
-    ${XPACK_DEVICE_FAMILY_COMPILE_DEFINITION}
-    
-    USE_HAL_DRIVER
-    OS_USE_SEMIHOSTING_SYSCALLS
+function(target_compile_definitions_platform target)
 
-    $<$<STREQUAL:"${CMAKE_BUILD_TYPE}","Debug">:OS_USE_TRACE_SEMIHOSTING_DEBUG>
-)
+  get_filename_component(PARENT_DIR ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
 
-set(platform_cpu 
+  target_compile_definitions(
+    ${target}
 
-  -mcpu=cortex-m4
-  -mthumb
-  -mfloat-abi=soft
-)
+    PRIVATE
+      ${XPACK_PLATFORM_COMPILE_DEFINITION}
+      ${XPACK_DEVICE_COMPILE_DEFINITION}
+      ${XPACK_DEVICE_FAMILY_COMPILE_DEFINITION}
+      
+      USE_HAL_DRIVER
+      OS_USE_SEMIHOSTING_SYSCALLS
 
-set(platform_common_options
+      $<$<STREQUAL:"${CMAKE_BUILD_TYPE}","Debug">:OS_USE_TRACE_SEMIHOSTING_DEBUG>
+  )
+endfunction()
 
-  -fmessage-length=0
-  -fsigned-char
-  -ffunction-sections
-  -fdata-sections
-  -fno-move-loop-invariants
+function(target_options_platform target)
 
-  $<$<COMPILE_LANGUAGE:CXX>:-fabi-version=0>
-  $<$<COMPILE_LANGUAGE:CXX>:-fno-exceptions>
-  $<$<COMPILE_LANGUAGE:CXX>:-fno-rtti>
-  $<$<COMPILE_LANGUAGE:CXX>:-fno-use-cxa-atexit>
-  $<$<COMPILE_LANGUAGE:CXX>:-fno-threadsafe-statics>
+  get_filename_component(PARENT_DIR ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
 
-  # -Wunused
-  # -Wuninitialized
-  # -Wall
-  # -Wextra
-  # -Wconversion
-  # -Wpointer-arith
-  # -Wshadow
-  # -Wlogical-op
-  # -Wfloat-equal
+  set(platform_cpu 
 
-  # $<$<COMPILE_LANGUAGE:CXX>:-Wctor-dtor-privacy>
-  # $<$<COMPILE_LANGUAGE:CXX>:-Wnoexcept>
-  # $<$<COMPILE_LANGUAGE:CXX>:-Wnon-virtual-dtor>
-  # $<$<COMPILE_LANGUAGE:CXX>:-Wstrict-null-sentinel>
-  # $<$<COMPILE_LANGUAGE:CXX>:-Wsign-promo>
-)
+    -mcpu=cortex-m4
+    -mthumb
+    -mfloat-abi=soft
+  )
 
-target_compile_options(
-  xpack-target
-  PRIVATE
-  
-    ${platform_cpu}
-    ${platform_common_options}
-)
+  set(platform_common_options
 
-target_link_options(
-  xpack-target
-  PRIVATE
-  
-    ${platform_cpu}
-    ${platform_common_options}
+    -fmessage-length=0
+    -fsigned-char
+    -ffunction-sections
+    -fdata-sections
+    -fno-move-loop-invariants
 
-    -nostartfiles
-    -specs=nano.specs
-    -Xlinker --gc-sections
-    -Wl,-Map,${CMAKE_PROJECT_NAME}.map
+    $<$<COMPILE_LANGUAGE:CXX>:-fabi-version=0>
+    $<$<COMPILE_LANGUAGE:CXX>:-fno-exceptions>
+    $<$<COMPILE_LANGUAGE:CXX>:-fno-rtti>
+    $<$<COMPILE_LANGUAGE:CXX>:-fno-use-cxa-atexit>
+    $<$<COMPILE_LANGUAGE:CXX>:-fno-threadsafe-statics>
 
-    # Use absolute paths, otherwise set -L.
-    -T${CMAKE_CURRENT_LIST_DIR}/../linker-scripts/mem.ld
-    -T${PROJECT_SOURCE_DIR}/xpacks/micro-os-plus-architecture-cortexm/linker-scripts/sections.ld
-)
+    # -Wunused
+    # -Wuninitialized
+    # -Wall
+    # -Wextra
+    # -Wconversion
+    # -Wpointer-arith
+    # -Wshadow
+    # -Wlogical-op
+    # -Wfloat-equal
+
+    # $<$<COMPILE_LANGUAGE:CXX>:-Wctor-dtor-privacy>
+    # $<$<COMPILE_LANGUAGE:CXX>:-Wnoexcept>
+    # $<$<COMPILE_LANGUAGE:CXX>:-Wnon-virtual-dtor>
+    # $<$<COMPILE_LANGUAGE:CXX>:-Wstrict-null-sentinel>
+    # $<$<COMPILE_LANGUAGE:CXX>:-Wsign-promo>
+  )
+
+  target_compile_options(
+    ${target}
+
+    PRIVATE
+      ${platform_cpu}
+      ${platform_common_options}
+  )
+
+  target_link_options(
+    ${target}
+
+    PRIVATE
+      ${platform_cpu}
+      ${platform_common_options}
+
+      -nostartfiles
+      -specs=nano.specs
+      -Xlinker --gc-sections
+      -Wl,-Map,${CMAKE_PROJECT_NAME}.map
+
+      # Use absolute paths, otherwise set -L.
+      -T${PARENT_DIR}/linker-scripts/mem.ld
+      -T${PROJECT_SOURCE_DIR}/xpacks/micro-os-plus-architecture-cortexm/linker-scripts/sections.ld
+  )
+
+endfunction()
 
 # -----------------------------------------------------------------------------
 
